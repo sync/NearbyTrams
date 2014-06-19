dfb16e0bc977f8782fec3ac29348186f36bc96f1
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-06-20 08:02:18 +1000

    added NearbyTramsStorageKit, a framework containing everything related to the domain layer and core data

---
+++ NearbyTramsStorageKit/Source/NearbyTramsStorageKit.h
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NearbyTramsStorageKit.
+FOUNDATION_EXPORT double NearbyTramsStorageKitVersionNumber;
+
+//! Project version string for NearbyTramsStorageKit.
+FOUNDATION_EXPORT const unsigned char NearbyTramsStorageKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NearbyTramsStorageKit/PublicHeader.h>
+
+
