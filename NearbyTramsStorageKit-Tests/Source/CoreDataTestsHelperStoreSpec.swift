3414c56835b0d77cf0202dd425a1bfc9731d555f
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-06-21 21:03:00 +1000

    added CoreDataTestsHelperStore and it's corresponding spec, a in memory core data store useful for tests

---
+++ NearbyTramsStorageKit-Tests/Source/CoreDataTestsHelperStoreSpec.swift
@@ -0,0 +1,24 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Quick
+import Nimble
+import NearbyTramsStorageKit
+
+class CoreDataTestsStoreSpec: QuickSpec {
+    override func spec() {
+        describe("Managed Object Context") {
+            var store: CoreDataTestsHelperStore!
+            
+            beforeEach {
+                store = CoreDataTestsHelperStore()
+            }
+            
+            it("should be non nil") {
+                let moc = store.managedObjectContext;
+                expect(moc).notTo.beNil()
+            }
+        }
+    }
+}
---
+++ NearbyTramsStorageKit/Source/CoreDataTestsHelperStore.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataTestsHelperStore
+{
+    @lazy var managedObjectContext: NSManagedObjectContext = {
+        
+        let managedObjectModel = CoreDataStackManager.sharedInstance.managedObjectModel
+        
+        var error: NSError? = nil
+        let persistentStoreCoordinator:NSPersistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: managedObjectModel)
+        if persistentStoreCoordinator.addPersistentStoreWithType(NSInMemoryStoreType,
+            configuration: nil,
+            URL: nil,
+            options: nil,
+            error: &error) == nil
+        {
+            println("there was an error loading in memory core data tests store: \(error!.localizedDescription)")
+            abort()
+        }
+        
+        let _managedObjectContext = NSManagedObjectContext()
+        _managedObjectContext.persistentStoreCoordinator = persistentStoreCoordinator
+        
+        
+        return _managedObjectContext
+        }()
+}
