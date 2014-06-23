5e2853a56d4801e107b80ee9e97c2c2a545b9c74
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-06-24 09:44:48 +1000

    added MockWebServiceURLProtocol, a NSURLProtocol subclass useful when wanting to mock network calls

---
+++ NearbyTramsNetworkKit-Tests/Source/MockWebServiceURLProtocolSpec.swift
@@ -0,0 +1,14 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Quick
+import Nimble
+import NearbyTramsNetworkKit
+
+class MockWebServiceURLProtocolSpec: QuickSpec {
+    override func spec() {
+        // FIXME: missing spec
+    }
+}
+
---
+++ NearbyTramsNetworkKit/Source/MockWebServiceURLProtocol.swift
@@ -0,0 +1,81 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Foundation
+
+struct MockWebServiceResponse
+{
+    let body: Dictionary<String, AnyObject>
+    let header: Dictionary<String, AnyObject>
+    let statusCode: Int
+    let error: NSError?
+    
+    init (body: Dictionary<String, AnyObject>, header: Dictionary<String, AnyObject>, statusCode: Int = 200, error: NSError?)
+    {
+        self.body = body
+        self.header = header
+        self.statusCode = statusCode
+        self.error = error
+    }
+}
+
+struct MockWebServiceResponseStorage
+{
+    static var storage: MockWebServiceResponse?
+}
+
+class MockWebServiceURLProtocol: NSURLProtocol
+{
+    class var cannedResponse:(response: MockWebServiceResponse!) {
+        get {
+             return MockWebServiceResponseStorage.storage
+        }
+        set {
+            MockWebServiceResponseStorage.storage = newValue
+        }
+    }
+    
+    override class func canInitWithRequest(request: NSURLRequest!) -> Bool
+    {
+        return request.URL.scheme == "mock"
+    }
+    
+    override class func canonicalRequestForRequest(request: NSURLRequest!) -> NSURLRequest!
+    {
+        return request
+    }
+    
+    override class func requestIsCacheEquivalent(a: NSURLRequest!, toRequest b: NSURLRequest!) -> Bool
+    {
+        return a.URL == b.URL
+    }
+    
+    override func startLoading()
+    {
+        let request = self.request
+        let client: NSURLProtocolClient = self.client
+        
+        let cannedResponse: MockWebServiceResponse = MockWebServiceURLProtocol.cannedResponse
+        
+        if let error = cannedResponse.error
+        {
+            client.URLProtocol(self, didFailWithError: error)
+        }
+        else
+        {
+            let response = NSHTTPURLResponse(URL: request.URL, statusCode: cannedResponse.statusCode, HTTPVersion: "HTTP/1.1", headerFields: cannedResponse.header)
+            client.URLProtocol(self, didReceiveResponse: response, cacheStoragePolicy: NSURLCacheStoragePolicy.NotAllowed)
+                        
+            let jsonData = NSJSONSerialization.dataWithJSONObject(cannedResponse.body, options: NSJSONWritingOptions(), error: nil)
+            client.URLProtocol(self, didLoadData: jsonData)
+            
+            client.URLProtocolDidFinishLoading(self)
+        }
+    }
+    
+    override func stopLoading()
+    {
+        
+    }
+}
