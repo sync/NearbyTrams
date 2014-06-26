7886bd787b9fb9b9e1acc7186e7fda88c84e998a
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-06-26 21:37:09 +1000

    added NearbyTramsKit, where all the iOS / OS X reusable code should go, will act as a mediator between the network and storage frameworks

---
+++ NearbyTramsKit/Source/NearbyTramsKit.h
@@ -0,0 +1,19 @@
+//
+//  NearbyTramsKit.h
+//  NearbyTramsKit
+//
+//  Created by Anthony Mittaz on 26/06/2014.
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NearbyTramsKit.
+FOUNDATION_EXPORT double NearbyTramsKitVersionNumber;
+
+//! Project version string for NearbyTramsKit.
+FOUNDATION_EXPORT const unsigned char NearbyTramsKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NearbyTramsKit/PublicHeader.h>
+
+
