5cd7fd11120eb4b145649528f11118b78b98259f
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-06-27 21:21:41 +1000

    added BrowseRoutesViewControllerModel, a model class that will be interacting with the tram browser view controller on OS X and iOS

---
+++ NearbyTramsKit-Tests/Source/BrowseRoutesViewControllerModelSpec.swift
@@ -0,0 +1,13 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Quick
+import Nimble
+import NearbyTramsKit
+
+class BrowseRoutesViewControllerModelSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
---
+++ NearbyTramsKit/Source/BrowseRoutesViewControllerModel.swift
@@ -0,0 +1,10 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Foundation
+
+class BrowseRoutesViewControllerModel
+{
+   
+}
