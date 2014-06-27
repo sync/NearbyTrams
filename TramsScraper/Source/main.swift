cc25665f33fb2086ffe4f66980c9ee2bbbb9674c
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-06-28 08:47:23 +1000

    linked command line app agains NearbyTramsKit, and using it's RoutesProvider to repopulate database with routes from the API

--- TramsScraper/Source/main.swift
+++ TramsScraper/Source/main.swift
@@ -6,6 +6,7 @@ import Foundation
 import NearbyTramsStorageKit
 import NearbyTramsNetworkKit
 import CoreData
+import NearbyTramsKit
 
 var managedObjectContext: NSManagedObjectContext = {
     let moc = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
@@ -13,71 +14,22 @@ var managedObjectContext: NSManagedObjectContext = {
     return moc
     }()
 
-let service = NetworkService()
-func getAllRoutesWithCompletionHandler(completionHandler: ((NSManagedObjectID[]?, NSError?) -> Void)?) -> Void
-{
-    let task = service.getAllRoutesWithCompletionHandler {
-        routes, error -> Void in
-        
-        if (error)
-        {
-            if let handler = completionHandler
-            {
-                dispatch_async(dispatch_get_main_queue()) {
-                    handler(nil, error)
-                }
-            }
-        }
-        else if (routes)
-        {
-            let localContext = NSManagedObjectContext(concurrencyType: .ConfinementConcurrencyType)
-            localContext.parentContext = managedObjectContext
-            
-            let routesObjectIds = routes!.map {
-                (routeDictionary: NSDictionary) -> NSManagedObjectID in
-                
-                let route = Route.insertInManagedObjectContext(localContext)
-                route.configureWithDictionaryFromRest(routeDictionary)
-                localContext.obtainPermanentIDsForObjects([route], error: nil)
-                
-                return route.objectID
-            }
-            
-            localContext.save(nil)
-            
-            if let handler = completionHandler
-            {
-                dispatch_async(dispatch_get_main_queue()) {
-                    handler(routesObjectIds, nil)
-                }
-            }
-        }
-    }
-}
-
 var shouldKeepRunning = true
-getAllRoutesWithCompletionHandler {
-    routesObjectIds, error -> Void in
+var provider = RoutesProvider(managedObjectContext: managedObjectContext)
+provider.getAllRoutesWithManagedObjectContext(managedObjectContext, {
+    routes, error -> Void in
     
     if (error)
     {
         println("there was an error dowloading all routes:r \(error!.localizedDescription)")
     }
-    else if (routesObjectIds)
+    else if (routes)
     {
-        let predicate = NSPredicate(format:"self IN %@", routesObjectIds!)
-        let request = NSFetchRequest(entityName: "Route")
-        request.predicate = predicate
-        
-        //func executeFetchRequest(request: NSFetchRequest!, error: NSErrorPointer) -> AnyObject[]!
-        if let routes = managedObjectContext.executeFetchRequest(request, error: nil)
-        {
-            println("got all routes: \(routes)")
-        }
+        println("got all routes: \(routes)")
     }
     
     shouldKeepRunning = false
-}
+    })
 
 do {
     NSRunLoop.currentRunLoop().runUntilDate(NSDate(timeIntervalSinceNow: 1))
