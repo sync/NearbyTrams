27168ea790c20a86bc2017bd589a2872d14a8696
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-07-04 17:31:30 +1000

    starded the rework of the command line app, using arguments to fetch additional values and print help and switched over to using a dispatch_group instead of a counter when keeeping runloop running

--- TramsScraper/Source/main.swift
+++ TramsScraper/Source/main.swift
@@ -8,73 +8,98 @@ import NearbyTramsNetworkKit
 import CoreData
 import NearbyTramsKit
 
-var managedObjectContext: NSManagedObjectContext = {
+func printHelpForAppNamed(appName: String)
+{
+    println("Usage: \(appName) [-6] [-o <path>] [-f <path>] [-p <prefix>] [<paths>]\n")
+    println("       \(appName) -h\n\n")
+    println("Options:\n")
+    println("    -d <path>      Database <path>, required\n")
+    println("    -stopInfo      Include additional stop info\n")
+    println("    -schedules     Include schedules\n")
+    println("    -h             Print this help and exit\n")
+}
+
+let arguments = Process.arguments
+
+let shouldPrintHelp = contains(arguments as String[], "-h")
+if shouldPrintHelp
+{
+    let appName = arguments[0].lastPathComponent
+    printHelpForAppNamed(appName)
+    
+    exit(0)
+}
+
+let shouldRequestStopInfo = contains(arguments as String[], "-stopInfo")
+let shouldRequestSchedules = contains(arguments as String[], "-schedules")
+
+let managedObjectContext: NSManagedObjectContext = {
     let moc = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
     moc.persistentStoreCoordinator = CoreDataStackManager.sharedInstance.persistentStoreCoordinator
     return moc
     }()
 
-var shouldKeepRunning = true
+let group = dispatch_group_create();
+
 var routesProvider = RoutesProvider(managedObjectContext: managedObjectContext)
 var stopsProvider = StopsProvider(managedObjectContext: managedObjectContext)
 
+dispatch_group_enter(group)
 routesProvider.getAllRoutesWithManagedObjectContext(managedObjectContext) {
     routeObjectIds, error -> Void in
     
-    if (error)
-    {
-        println("there was an error dowloading all routes: \(error!.localizedDescription)")
-    }
-    else if (routeObjectIds)
+    if (routeObjectIds)
     {
         println("found routes: \(routeObjectIds!.count)")
         
-        if routeObjectIds!.isEmpty
+        let fetchedRoutes: (routes: Route[]?, error:NSError?) = Route.fetchAllForManagedObjectIds(routeObjectIds!, usingManagedObjectContext: managedObjectContext)
+        if let routes = fetchedRoutes.routes
         {
-            shouldKeepRunning = false
-        }
-        else
-        {
-            let fetchedRoutes: (routes: Route[]?, error:NSError?) = Route.fetchAllForManagedObjectIds(routeObjectIds!, usingManagedObjectContext: managedObjectContext)
-            if let routes = fetchedRoutes.routes
+            for (index, route) in enumerate(routes)
             {
-                var counter = routes.count
-                for (index, route) in enumerate(routes)
+                if let routeNo = route.routeNo
                 {
-                    if let routeNo = route.routeNo
-                    {
-                        stopsProvider.getStopsWithRouteNo(routeNo as Int, requestStopInfo: true, managedObjectContext: managedObjectContext) {
-                            stopObjectIds, error -> Void in
+                    dispatch_group_enter(group)
+                    stopsProvider.getStopsWithRouteNo(routeNo as Int, requestStopInfo: shouldRequestStopInfo, managedObjectContext: managedObjectContext) {
+                        stopObjectIds, error -> Void in
+                        
+                        if (stopObjectIds)
+                        {
+                            println("found stops:\(stopObjectIds!.count) for route: \(routeNo), up destination: \(route.isUpDestination)")
                             
-                            if (error)
+                            let fetchedStops: (stops: Stop[]?, error:NSError?) = Stop.fetchAllForManagedObjectIds(stopObjectIds!, usingManagedObjectContext: managedObjectContext)
+                            if let stops = fetchedStops.stops
                             {
-                                println("there was an error dowloading all stops for route \(routeNo): \(error!.localizedDescription)")
+                                route.stops = NSMutableSet(array: stops)
+                                managedObjectContext.save(nil)
                             }
-                            else if (stopObjectIds)
-                            {
-                                println("found stops:\(stopObjectIds!.count) for route: \(routeNo)")
-                                
-                                let fetchedStops: (stops: Stop[]?, error:NSError?) = Stop.fetchAllForManagedObjectIds(stopObjectIds!, usingManagedObjectContext: managedObjectContext)
-                                if let stops = fetchedStops.stops
-                                {
-                                    route.stops = NSMutableSet(array: stops)
-                                    managedObjectContext.save(nil)
-                                }
-                            }
-                            
-                            --counter
-                            shouldKeepRunning = counter != 0
                         }
-                    }
-                    else
-                    {
-                        --counter
-                        shouldKeepRunning = counter != 0
+                        else
+                        {
+                            println("there was an error dowloading all stops for route \(routeNo): \(error?.localizedDescription)")
+                        }
+                        
+                        dispatch_group_leave(group)
                     }
                 }
             }
-
         }
+    }
+    else
+    {
+        println("there was an error dowloading all routes: \(error?.localizedDescription)")
+    }
+    
+    dispatch_group_leave(group)
+}
+
+var shouldKeepRunning = true
+
+dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0)) {
+    dispatch_group_wait(group, DISPATCH_TIME_FOREVER);
+    
+    dispatch_async(dispatch_get_main_queue()) {
+        shouldKeepRunning = false
     }
 }
 
