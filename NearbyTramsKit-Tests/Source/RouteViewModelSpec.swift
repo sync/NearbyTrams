511e62011e3a1e194bea32cdfdbf61a391ec10f7
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-07-06 17:07:03 +1000

    introduced RouteViewModel, bridging the route managed objects with it's views

---
+++ NearbyTramsKit-Tests/Source/RouteViewModelSpec.swift
@@ -0,0 +1,32 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Quick
+import Nimble
+import NearbyTramsKit
+
+class RouteViewModelSpec: QuickSpec {
+    override func spec() {
+        
+        var viewModel: RouteViewModel!
+        
+        describe("init") {
+            context("when given a color") {
+                var color: CGColorRef!
+                
+                beforeEach {
+                    color = CGColorCreateGenericGray(0.5, 1.0)
+                    viewModel = RouteViewModel(identifier: "an identifier", routeNo: 76, color: color)
+                }
+                
+                it ("should remember it") {
+                    // FIXME: hack for test to compile
+                    let modelColors: NSArray = [viewModel.color]
+                    let colors: NSArray = [color!]
+                    expect(modelColors).to.equal(colors)
+                }
+            }
+        }
+    }
+}
---
+++ NearbyTramsKit/Source/RouteViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Foundation
+import CoreFoundation
+
+class RouteViewModel
+{
+    let identifier: String
+    let routeNo: Int
+    let color: CGColorRef
+    
+    init (identifier: String, routeNo: Int, color: CGColorRef = CGColorCreateGenericRGB(CGFloat(arc4random_uniform(255)) / 255.0, CGFloat(arc4random_uniform(255)) / 255.0, CGFloat(arc4random_uniform(255)) / 255.0, 1.0))
+    {
+        self.identifier = identifier
+        self.routeNo = routeNo
+        self.color = color
+    }
+}
