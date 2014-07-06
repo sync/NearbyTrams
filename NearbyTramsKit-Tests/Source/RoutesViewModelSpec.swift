3d4b4241d668ffd639c013552b3ee7117a27206c
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-07-06 23:06:15 +1000

    added RoutesViewModel, fetching managed objects from the core data store and converting them to route view models, also tracking changed routes (added / deleted / updated)

---
+++ NearbyTramsKit-Tests/Source/RoutesViewModelSpec.swift
@@ -0,0 +1,302 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Quick
+import Nimble
+import NearbyTramsKit
+import NearbyTramsNetworkKit
+import NearbyTramsStorageKit
+
+class FakeRoutesViewModelDelegate: RoutesViewModelDelegate
+{
+    var addedRoutes: RouteViewModel[]?
+    var removedRoutes: RouteViewModel[]?
+    var updatedRoutes: RouteViewModel[]?
+    
+    func routesViewModelDidAddRoutes(routesViewModel: RoutesViewModel, routes: RouteViewModel[])
+    {
+        addedRoutes = routes
+    }
+    
+    func routesViewModelDidRemoveRoutes(routesViewModel: RoutesViewModel, routes: RouteViewModel[])
+    {
+        removedRoutes = routes
+    }
+    
+    func routesViewModelDidUpdateRoutes(routesViewModel: RoutesViewModel, routes: RouteViewModel[])
+    {
+        updatedRoutes = routes
+    }
+}
+
+class RoutesViewModelSpec: QuickSpec {
+    override func spec() {
+        
+        var store: CoreDataTestsHelperStore!
+        var moc: NSManagedObjectContext!
+        
+        var fakeDelegate: FakeRoutesViewModelDelegate!
+        var viewModel: RoutesViewModel!
+        
+        var fetchRequest: NSFetchRequest!
+        
+        beforeEach {
+            store = CoreDataTestsHelperStore()
+            moc = store.managedObjectContext
+            
+            viewModel = RoutesViewModel(managedObjectContext: moc)
+            fakeDelegate = FakeRoutesViewModelDelegate()
+            viewModel.delegate = fakeDelegate
+            
+            fetchRequest = NSFetchRequest(entityName: Route.entityName)
+            viewModel.startUpdatingRoutesWithFetchRequest(fetchRequest)
+        }
+        
+        afterEach {
+            let success = moc.save(nil)
+        }
+        
+        context("when empty") {
+            it("should have no routes") {
+                expect(viewModel.routes).to.beEmpty()
+            }
+            
+            it("should have per routes count zero") {
+                expect(viewModel.routesCount).to.equal(0)
+            }
+        }
+        
+        describe("Adding routes") {
+            context("when adding one route") {
+                beforeEach {
+                    let route: Route = Route.insertInManagedObjectContext(moc)
+                    route.routeNo = 123
+                    route.composedKey = "123-true"
+                    moc.save(nil)
+                }
+                
+                it("should have one routes") {
+                    expect(viewModel.routes.count).to.equal(1)
+                }
+                
+                it("should have per routes count one") {
+                    expect(viewModel.routesCount).to.equal(1)
+                }
+                
+                it("should tell delegate about the added route") {
+                    expect(fakeDelegate.addedRoutes![0].identifier).to.equal("123-true")
+                }
+            }
+            
+            context("when adding one invalid route") {
+                beforeEach {
+                    let route: Route = Route.insertInManagedObjectContext(moc)
+                    moc.save(nil)
+                }
+                
+                it("should have no routes") {
+                    expect(viewModel.routes).to.beEmpty()
+                }
+                
+                it("should have per routes count zero") {
+                    expect(viewModel.routesCount).to.equal(0)
+                }
+            }
+            
+            context("when adding multiple routes") {
+                beforeEach {
+                    let route1: Route = Route.insertInManagedObjectContext(moc)
+                    route1.routeNo = 123
+                    route1.composedKey = "123-true"
+                    
+                    let route2: Route = Route.insertInManagedObjectContext(moc)
+                    route2.routeNo = 456
+                    route2.composedKey = "456-false"
+                    
+                    moc.save(nil)
+                }
+                
+                it("should have one routes") {
+                    expect(viewModel.routes.count).to.equal(2)
+                }
+                
+                it("should have per routes count one") {
+                    expect(viewModel.routesCount).to.equal(2)
+                }
+                
+                it("should tell delegate about the two added route") {
+                    let identifiers = [fakeDelegate.addedRoutes![0].identifier, fakeDelegate.addedRoutes![1].identifier]
+                    expect(identifiers).to.contain("123-true")
+                    expect(identifiers).to.contain("456-false")
+                }
+            }
+        }
+        
+        describe("Removing routes") {
+            var route1: Route!
+            var route2: Route!
+            
+            beforeEach {
+                route1 = Route.insertInManagedObjectContext(moc) as Route
+                route1.routeNo = 123
+                route1.composedKey = "123-true"
+                
+                route2 = Route.insertInManagedObjectContext(moc) as Route
+                route2.routeNo = 456
+                route2.composedKey = "456-false"
+                
+                moc.save(nil)
+            }
+            
+            context("when removing one route") {
+                beforeEach {
+                    moc.deleteObject(route1)
+                    moc.save(nil)
+                }
+                
+                it("should have one routes") {
+                    expect(viewModel.routes[0].identifier).to.equal("456-false")
+                }
+                
+                it("should have per routes count one") {
+                    expect(viewModel.routesCount).to.equal(1)
+                }
+                
+                it("should tell delegate about the removed route") {
+                     expect(fakeDelegate.removedRoutes![0].identifier).to.equal("123-true")
+                }
+            }
+            
+            context("when removing multiple routes") {
+                beforeEach {
+                    moc.deleteObject(route1)
+                    moc.deleteObject(route2)
+                    moc.save(nil)
+                }
+                
+                it("should have one routes") {
+                    expect(viewModel.routes.count).to.equal(0)
+                }
+                
+                it("should have per routes count one") {
+                    expect(viewModel.routesCount).to.equal(0)
+                }
+                
+                it("should tell delegate about the two removed route") {
+                    let identifiers = [fakeDelegate.removedRoutes![0].identifier, fakeDelegate.removedRoutes![1].identifier]
+                    expect(identifiers).to.contain("123-true")
+                    expect(identifiers).to.contain("456-false")
+                }
+            }
+        }
+        
+        describe("Updating routes") {
+            var route1: Route!
+            var route2: Route!
+            
+            beforeEach {
+                route1 = Route.insertInManagedObjectContext(moc) as Route
+                route1.routeNo = 123
+                route1.composedKey = "123-true"
+                
+                route2 = Route.insertInManagedObjectContext(moc) as Route
+                route2.routeNo = 456
+                route2.composedKey = "456-false"
+                
+                moc.save(nil)
+            }
+            
+            context("when updating one route") {
+                beforeEach {
+                    route1.destination = "new destination"
+                    
+                    moc.save(nil)
+                }
+                
+                it("should tell delegate about the updated route") {
+                     expect(fakeDelegate.updatedRoutes![0].identifier).to.equal("123-true")
+                }
+            }
+            
+            context("when updating multiple routes") {
+                beforeEach {
+                    route1.destination = "new destination"
+                    route2.destination = "another new destination"
+                    
+                    moc.save(nil)
+                }
+                
+                it("should tell delegate about the two updated route") {
+                    let identifiers = [fakeDelegate.updatedRoutes![0].identifier, fakeDelegate.updatedRoutes![1].identifier]
+                    expect(identifiers).to.contain("123-true")
+                    expect(identifiers).to.contain("456-false")
+                }
+            }
+        }
+        
+        
+        describe("stopUpdatingRoutes") {
+            beforeEach {
+                viewModel.stopUpdatingRoutes()
+                
+                let route1: Route = Route.insertInManagedObjectContext(moc)
+                route1.routeNo = 123
+                route1.composedKey = "123-true"
+                moc.save(nil)
+            }
+            
+            it("should not tell delegate about the two added route") {
+                expect(fakeDelegate.addedRoutes?.count).to.beNil()
+            }
+        }
+        
+        describe("reconfiguring startUpdatingRoutesWithFetchRequest") {
+            context("when adding one route") {
+                beforeEach {
+                    let route1: Route = Route.insertInManagedObjectContext(moc)
+                    route1.routeNo = 123
+                    route1.composedKey = "123-true"
+                    
+                    let route2: Route = Route.insertInManagedObjectContext(moc)
+                    route2.routeNo = 456
+                    route2.composedKey = "456-false"
+                    
+                    moc.save(nil)
+                    
+                    fetchRequest = NSFetchRequest(entityName: Route.entityName)
+                    fetchRequest.predicate = NSPredicate(format:"composedKey == %@", "123-true")
+                    viewModel.startUpdatingRoutesWithFetchRequest(fetchRequest)
+                    
+                    let route3: Route = Route.insertInManagedObjectContext(moc)
+                    route3.routeNo = 789
+                    route3.composedKey = "789-true"
+                    
+                    moc.save(nil)
+                }
+                
+                it("should have one routes") {
+                    expect(viewModel.routes.count).to.equal(1)
+                }
+                
+                it("should have per routes count one") {
+                    expect(viewModel.routesCount).to.equal(1)
+                }
+                
+                it("should tell delegate about the added routes") {
+                    let identifiers = [fakeDelegate.addedRoutes![0].identifier, fakeDelegate.addedRoutes![1].identifier]
+                    expect(identifiers).to.contain("123-true")
+                    expect(identifiers).to.contain("456-false")
+                }
+                
+                it("should tell delegate about the updated route") {
+                     expect(fakeDelegate.updatedRoutes![0].identifier).to.equal("123-true")
+                }
+                
+                it("should tell delegate about the removed route") {
+                    expect(fakeDelegate.removedRoutes![0].identifier).to.equal("456-false")
+                }
+            }
+        }
+    }
+}
---
+++ NearbyTramsKit/Source/RoutesViewModel.swift
@@ -0,0 +1,271 @@
+//
+//  Copyright (c) 2014 Dblechoc. All rights reserved.
+//
+
+import Foundation
+import NearbyTramsStorageKit
+
+protocol RoutesViewModelDelegate
+{
+    func routesViewModelDidAddRoutes(routesViewModel: RoutesViewModel, routes: RouteViewModel[])
+    func routesViewModelDidRemoveRoutes(routesViewModel: RoutesViewModel, routes: RouteViewModel[])
+    func routesViewModelDidUpdateRoutes(routesViewModel: RoutesViewModel, routes: RouteViewModel[])
+}
+
+class RoutesViewModel: NSObject, SNRFetchedResultsControllerDelegate
+{
+    var delegate: RoutesViewModelDelegate?
+    
+    let managedObjectContext: NSManagedObjectContext
+    
+    var fetchedResultsController: SNRFetchedResultsController?
+    var routesStorage: Dictionary<String, RouteViewModel> = [ : ]
+    var currentChangeSet: FetchedResultsControllerChangeSet?
+    
+    init (managedObjectContext: NSManagedObjectContext)
+    {
+        self.managedObjectContext = managedObjectContext
+    }
+    
+    var routes: RouteViewModel[] {
+        return Array(routesStorage.values)
+    }
+    
+    var routesCount: Int {
+        return routes.count
+    }
+    
+    func startUpdatingRoutesWithFetchRequest(fetchRequest: NSFetchRequest)
+    {
+        let currentFetchedResultsController = SNRFetchedResultsController(managedObjectContext: managedObjectContext, fetchRequest: fetchRequest)
+        currentFetchedResultsController.delegate = self
+        
+        var error: NSError?
+        if currentFetchedResultsController.performFetch(&error)
+        {
+            if let fetchedRoutes = currentFetchedResultsController.fetchedObjects as? Route[]
+            {
+                synchronizeRoutesWithObjectsFromArray(fetchedRoutes)
+            }
+        }
+        else
+        {
+            println("Failed to perform fetch routes with request '\(fetchRequest)': \(error)");
+        }
+        
+        self.fetchedResultsController = currentFetchedResultsController
+    }
+    
+    func stopUpdatingRoutes()
+    {
+        if let currentFetchedResultsController = fetchedResultsController
+        {
+            currentFetchedResultsController.delegate = nil
+            
+        }
+        self.fetchedResultsController = nil
+    }
+    
+    func existingModelForRoute(route: Route) -> RouteViewModel?
+    {
+        if let identifier = route.uniqueIdentifier
+        {
+            return routesStorage[identifier]
+        }
+        
+        return nil
+    }
+    
+    func synchronizeRoutesWithObjectsFromArray(array: Route[])
+    {
+        var routesByIdentifier: Dictionary<String, Route> = [ : ]
+        for route in array
+        {
+            if let identifier = route.uniqueIdentifier
+            {
+                routesByIdentifier[identifier] = route
+            }
+        }
+        
+        var currentIdentifiers = NSSet(array: Array(routesStorage.keys))
+        var newIdentifiers =  NSSet(array: Array(routesByIdentifier.keys))
+        
+        let identifiersToAdd = NSMutableSet(set: newIdentifiers)
+        identifiersToAdd.minusSet(currentIdentifiers)
+        
+        let identifiersToRemove = NSMutableSet(set: currentIdentifiers)
+        identifiersToRemove.minusSet(newIdentifiers)
+        
+        let identifiersToUpdate = NSMutableSet(set: currentIdentifiers)
+        identifiersToUpdate.intersectSet(newIdentifiers)
+        
+        if identifiersToAdd.allObjects
+        {
+            var addedObjects: Route[] = []
+            for identifier : AnyObject in identifiersToRemove.allObjects
+            {
+                let route = routesByIdentifier[identifier as String]
+                if route
+                {
+                    addedObjects.append(route!)
+                }
+            }
+            addRoutesForObjects(addedObjects)
+        }
+        
+        if identifiersToRemove.allObjects
+        {
+            var removedObjects: Route[] = []
+            for identifier : AnyObject in identifiersToRemove.allObjects
+            {
+                let routeViewModel = routesStorage[identifier as String]
+                if routeViewModel
+                {
+                    var result: (route: Route?, error:NSError?) = Route.fetchOneForPrimaryKeyValue(routeViewModel!.identifier, usingManagedObjectContext: managedObjectContext)
+                    if let route = result.route
+                    {
+                        removedObjects.append(route)
+                    }
+                }
+            }
+            removeRoutesForObjects(removedObjects)
+        }
+        
+        if identifiersToUpdate.allObjects
+        {
+            var updatedObjects: Route[] = []
+            for identifier : AnyObject in identifiersToUpdate.allObjects
+            {
+                let route = routesByIdentifier[identifier as String]
+                if route
+                {
+                    updatedObjects.append(route!)
+                }
+            }
+            updateRoutesForObjects(updatedObjects)
+        }
+    }
+    
+    func addRoutesForObjects(array: Route[])
+    {
+        if !array.isEmpty
+        {
+            let routesViewModel = array.filter {
+                route -> Bool in
+                
+                return route.uniqueIdentifier != nil
+                }.map {
+                    route -> RouteViewModel in
+                    
+                    assert(self.existingModelForRoute(route) == nil, "route should not already exist!")
+                    
+                    let identifier = route.uniqueIdentifier!
+                    let viewModel = RouteViewModel(identifier: identifier, routeNo: Int(route.routeNo!))
+                    self.routesStorage[identifier] = viewModel
+                    return viewModel
+            }
+            
+            didAddRoutes(routesViewModel)
+        }
+    }
+    
+    func updateRoutesForObjects(array: Route[])
+    {
+        if !array.isEmpty
+        {
+            var updatedRoutes: RouteViewModel[] = []
+            for route in array {
+                if let existingRouteModel = existingModelForRoute(route )
+                {
+                    updatedRoutes.append(existingRouteModel)
+                }
+            }
+            
+            if !updatedRoutes.isEmpty
+            {
+                didUpdateRoutes(updatedRoutes)
+            }
+        }
+    }
+    
+    func removeRoutesForObjects(array: Route[])
+    {
+        if !array.isEmpty
+        {
+            var removedRoutes: RouteViewModel[] = []
+            for route in array {
+                if let existingRouteModel = existingModelForRoute(route )
+                {
+                    removedRoutes.append(existingRouteModel)
+                    routesStorage.removeValueForKey(existingRouteModel.identifier)
+                }
+            }
+            
+            if !removedRoutes.isEmpty
+            {
+                didRemoveRoutes(removedRoutes)
+            }
+        }
+    }
+    
+    func didAddRoutes(routes: RouteViewModel[])
+    {
+        self.delegate?.routesViewModelDidAddRoutes(self, routes: routes)
+    }
+    
+    func didRemoveRoutes(routes: RouteViewModel[])
+    {
+        self.delegate?.routesViewModelDidRemoveRoutes(self, routes: routes)
+    }
+    
+    func didUpdateRoutes(routes: RouteViewModel[])
+    {
+        self.delegate?.routesViewModelDidUpdateRoutes(self, routes: routes)
+    }
+    
+    func controllerWillChangeContent(controller: SNRFetchedResultsController!)
+    {
+        if let changeSet = currentChangeSet
+        {
+            updateModelWithChangeSet(changeSet)
+        }
+        
+        currentChangeSet = FetchedResultsControllerChangeSet();
+    }
+    
+    func controller(controller: SNRFetchedResultsController!, didChangeObject anObject: AnyObject!, atIndex index: Int, forChangeType type: SNRFetchedResultsChangeType, newIndex: Int)
+    {
+        if controller == fetchedResultsController
+        {
+            currentChangeSet?.addChange(FetchedResultsControllerChange(changedObject: anObject, atIndexPath: NSIndexPath(index: index), forChangeType: type, newIndexPath: NSIndexPath(index: newIndex)))
+        }
+    }
+    
+    func controllerDidChangeContent(controller: SNRFetchedResultsController!)
+    {
+        if let changeSet = currentChangeSet
+        {
+            updateModelWithChangeSet(changeSet)
+        }
+        
+        currentChangeSet = nil
+    }
+    
+    func updateModelWithChangeSet(changeSet: FetchedResultsControllerChangeSet)
+    {
+        if let addedObjecst = changeSet.allAddedObjects as? Route[]
+        {
+            addRoutesForObjects(addedObjecst)
+        }
+        
+        if let updatedObjecst = changeSet.allUpdatedObjects as? Route[]
+        {
+            updateRoutesForObjects(updatedObjecst)
+        }
+        
+        if let removedObjecst = changeSet.allRemovedObjects as? Route[]
+        {
+            removeRoutesForObjects(removedObjecst)
+        }
+    }
+}
