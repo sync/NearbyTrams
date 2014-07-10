e33e7754d97a639da4d9957489e22146776ce396
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-07-10 13:07:34 +1000

    fetch the latest schedules for a given stop number, and change LIFX bulb color according to how minutes away next schedule is

--- NearbyTramsApp/Source/ViewController.swift
+++ NearbyTramsApp/Source/ViewController.swift
@@ -4,22 +4,42 @@
 
 import Cocoa
 import LIFXOSX
+import NearbyTramsKit
+import NearbyTramsStorageKit
+import CoreData
 
-class ViewController: NSViewController, LFXNetworkContextObserver, LFXLightCollectionObserver, LFXLightObserver
+class ViewController: NSViewController, NearbyStopsViewControllerModelDelegate, LFXNetworkContextObserver, LFXLightCollectionObserver, LFXLightObserver
 {
+    let stopsViewModel: StopsViewModel
+    let schedulesRepository: SchedulesRepository
+    let nearbyStopsViewControllerModel: NearbyStopsViewControllerModel
+    
+    var schedule: Schedule?
+    
     let lifxNetworkContext: LFXNetworkContext
     var lights: LFXLight[]?
-    var taggedLightCollections: LFXLightCollection[]?
+    
+    let managedObjectContext: NSManagedObjectContext = {
+        let moc = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        moc.persistentStoreCoordinator = CoreDataStackManager.sharedInstance.persistentStoreCoordinator
+        return moc
+        }()
     
     init(coder: NSCoder!)
     {
+        let stopsViewModel = StopsViewModel(managedObjectContext: managedObjectContext)
+        
+        let provider = SchedulesProvider(managedObjectContext: managedObjectContext)
+        let schedulesRepository = SchedulesRepository(stopIdentifier: "2166", schedulesProvider: provider, managedObjectContext: managedObjectContext)
+        self.nearbyStopsViewControllerModel = NearbyStopsViewControllerModel(viewModel: stopsViewModel, repository: schedulesRepository)
         self.lifxNetworkContext = LFXClient.sharedClient().localNetworkContext
+        
+        self.stopsViewModel = stopsViewModel
+        self.schedulesRepository = schedulesRepository
+        
         super.init(coder: coder)
-    }
-    
-    override func awakeFromNib()
-    {
-        super.awakeFromNib()
+        
+        self.nearbyStopsViewControllerModel.delegate = self
         
         self.lifxNetworkContext.addNetworkContextObserver(self)
         self.lifxNetworkContext.allLightsCollection.addLightCollectionObserver(self)
@@ -31,33 +51,84 @@ class ViewController: NSViewController, LFXNetworkContextObserver, LFXLightColle
         
         self.updateTitle()
         self.updateLights()
-        self.updateTags()
+    }
+    
+    func colorForCurrentSchedule(schedule: Schedule?) -> LFXHSBKColor
+    {
+        // blue
+        var color: LFXHSBKColor = LFXHSBKColor(hue: 240, saturation: 0.98, brightness: 1.0)
+        
+        if let aSchedule = self.schedule
+        {
+            if let tramDate: NSDate = aSchedule.predictedArrivalDateTime
+            {
+                let differenceInSeconds = tramDate.timeIntervalSinceNow
+                let differenceInMinutes = Int(differenceInSeconds / 60)
+                switch differenceInMinutes {
+                case 0..2:
+                    // red
+                    color = LFXHSBKColor(hue: 0, saturation: 0.98, brightness: 1.0)
+                case 2..3:
+                    // orange
+                    color = LFXHSBKColor(hue: 34, saturation: 0.98, brightness: 1.0)
+                case 3..6:
+                    // green
+                    color = LFXHSBKColor(hue: 131, saturation: 0.98, brightness: 1.0)
+                default:
+                    break
+                }
+            }
+        }
+        
+        return color
     }
     
     func updateLights()
     {
         self.lights = self.lifxNetworkContext.allLightsCollection.lights as? LFXLight[]
-        // FIXME: reload tv data
-        
         if self.lights?.count > 0
         {
             let light: LFXLight = self.lights![0]
             
-            let color: LFXHSBKColor = LFXHSBKColor(hue: 0.7, saturation: 0.5, brightness: 0.1)
+            let color = self.colorForCurrentSchedule(self.schedule)
             light.setColor(color)
         }
     }
     
-    func updateTags()
-    {
-        self.taggedLightCollections = self.lifxNetworkContext.taggedLightCollections as? LFXLightCollection[]
-        // FIXME: reload tv data
-    }
     
     func updateTitle()
     {
         let status = self.lifxNetworkContext.isConnected ? "connected" : "searching"
         self.title = "LIFX Browser \(status)"
+    }
+    
+    // NearbyStopsViewControllerModelDelegate
+    func nearbyStopsViewControllerModelDidFinishLoading(nearbyStopsViewControllerModel: NearbyStopsViewControllerModel)
+    {
+        println("updated schedules")
+        
+        let fetchRequest = NSFetchRequest(entityName: Schedule.entityName)
+        fetchRequest.fetchLimit = 1
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "predictedArrivalDateTime", ascending: true)]
+        fetchRequest.predicate = NSPredicate(format:"stop.uniqueIdentifier == %@", "2166")
+        
+        let schedules = self.managedObjectContext.executeFetchRequest(fetchRequest, error: nil)
+        if !schedules.isEmpty
+        {
+            self.schedule = schedules[0] as? Schedule
+            
+            if let date: NSDate = schedules[0].predictedArrivalDateTime
+            {
+                let formattedDate = date.descriptionWithLocale(NSLocale.currentLocale())
+                println("next trame is at: \(formattedDate)")
+            }
+        }
+        else
+        {
+            self.schedule = nil
+        }
+        
+        self.updateLights()
     }
     
     //LFXNetworkContextObserver
@@ -78,14 +149,12 @@ class ViewController: NSViewController, LFXNetworkContextObserver, LFXLightColle
     {
         println("Network Context Did Add Tagged Light Collection: \(collection.tag)")
         collection.addLightCollectionObserver(self)
-        self.updateTags()
     }
     
     func networkContext(networkContext: LFXNetworkContext!, didRemoveTaggedLightCollection collection: LFXTaggedLightCollection!)
     {
         println("Network Context Did Add Removed Light Collection: \(collection.tag)")
         collection.removeLightCollectionObserver(self)
-        self.updateTags()
     }
     
     // LFXLightCollectionObserver
@@ -109,14 +178,12 @@ class ViewController: NSViewController, LFXNetworkContextObserver, LFXLightColle
     {
         println("Light: \(light) Did Change Label: \(label)");
     }
-
+    
     override var representedObject: AnyObject? {
-        didSet {
+    didSet {
         // Update the view, if already loaded.
-        }
-                                    
     }
-
-
+    
+    }
 }
 
