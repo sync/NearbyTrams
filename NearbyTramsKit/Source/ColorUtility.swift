709c69cbaa472f3077515ad3fc6ab14da54ba568
Anthony Mittaz <anthony.mittaz@rea-group.com>
2014-07-30 07:58:48 +1000

    fixed ColorUtility, it was no longer generating a random color

--- NearbyTramsKit/Source/ColorUtility.swift
+++ NearbyTramsKit/Source/ColorUtility.swift
@@ -8,6 +8,10 @@ public enum ColorUtility
 {
     public static func generateRandomColor() -> CGColorRef
     {
-        return CGColorCreateGenericRGB(0.0, 0.0, 0.0, 1.0)
+        let red = CGFloat(Int(arc4random_uniform(255))) / 255.0
+        let green = CGFloat(Int(arc4random_uniform(255))) / 255.0
+        let blue = CGFloat(Int(arc4random_uniform(255))) / 255.0
+        
+        return CGColorCreateGenericRGB(red, green, blue, 1.0)
     }
 }
